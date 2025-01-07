22f5d7b05042f371b9377cb23d43536494be9d92
alv6s <alvescvlp@gmail.com>
2025-01-07 20:27:40 +0000

    change2

--- translations.js
+++ translations.js
@@ -1,6 +1,6 @@
 const translations = {
     pt: {
-        home: "Bem-vindo ao Meu Site",
+        home: "Cenários APP",
         whatWeDo: "O que fazemos?",
         description: "Aqui está uma breve descrição do que fazemos. Nós fornecemos um serviço incrível e fácil de usar.",
         terms: "Termos e Condições",
@@ -8,7 +8,7 @@ const translations = {
         appointments: "Marcações",
     },
     en: {
-        home: "Welcome to My Website",
+        home: "Cenários APP",
         whatWeDo: "What we do?",
         description: "Here is a brief description of what we do. We provide an amazing and easy-to-use service.",
         terms: "Terms and Conditions",
